16d74142060e0afaa46df0f469e6e4b9bd6b7024
Chaitanya Kothakota <kcbgangadhar@gmail.com>
2024-04-22 13:07:18 +0530

    Navbar Navigation Updated

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -29,6 +29,22 @@
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
+});
+  
+
 
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
