5056b1c09d93c477e59031711fbc370ab48911c5
suresh522 <kajasuresh522@gmail.com>
2024-05-06 17:30:44 +0530

    changes done

--- static/assets/js/contact.js
+++ static/assets/js/contact.js
@@ -1,25 +1,52 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function hello(){
   // event.preventdefult();
   debugger
-  let fname = document.querySelector('#name').value;
+  let name = document.querySelector('#name').value;
   let email = document.querySelector('#email').value;
+  let phone = document.querySelector('#phone').value;
   let subject = document.querySelector('#subject').value;
   let message = document.querySelector('#message').value;
   
-  let mes = "Name:" + fname + "<br/> For email:" +  email + "<br/> Subject:" + subject + "<br/> Message:" + message
+  let mess = "Name:" + name + "<br/> For Email:" +  email + "<br/> Phone:" + phone + "<br/> Subject:" + subject + "<br/> Message:" + message
    
-  Email.send({
-      Host : "smtp.elasticemail.com",
-      Username : "aperturart@gmail.com",
-      Password : "6F7C8AC4D52994A9B70E2EB933EB0D7BE19B",
-      To : 'priya4241honey@gmail.com',
-      From : "aperturart@gmail.com",
-      Subject : subject,
-      Body : mes
-  }).then(
-    alert("sucessfully sending email!")
-  );
-  
-}
-    
+
+const serviceID = "service_acvejvb";
+const templateID = "template_hzgo3kx";
+
+emailjs.send(serviceID, templateID, mess)
+  .then(function (response) {
+    console.log('Email sent successfully:', response.status, response.text);
+    alert("Thank you, " + feedbackData.name + "! Your message has been sent successfully.");
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('feedback').value = '';
+    document.getElementById('suggestions').value = '';
+    document.querySelector('.star-rating').setAttribute('data-rating', '');
+    document.querySelectorAll('.star-rating .star').forEach(star => star.classList.remove('selected'));
+    document.querySelectorAll('.feedback-options button').forEach(button => button.classList.remove('active'));
+  }, function (error) {
+    console.error("Email sending failed:", error);
+    alert("Sorry ...!, " + feedbackData.name + ". Oops! Something went wrong. Please try again later.");
+  });
+} 
+
+
+
